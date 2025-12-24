61c1104e59c15785ea6b76228f9e44734c328b8f
Clianex <clianex.ia@gmail.com>
2025-12-24 18:53:34 +0100

    Add global CSS and update body class

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,11 @@
+import "./globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body className="min-h-screen">
+        {children}
+      </body>
     </html>
   );
 }
-
