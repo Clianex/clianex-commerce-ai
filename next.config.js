cc58be8777dff26f31e745717c229eef211a6036
Clianex <clianex.ia@gmail.com>
2025-12-25 12:13:14 +0100

    Add experimental appDir configuration
    Enable experimental app directory feature in Next.js.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true, // Habilita el App Router en Next.js
+  },
 };
 
 module.exports = nextConfig;
-
