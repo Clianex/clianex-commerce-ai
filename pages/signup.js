b2666dd15d49af28aa9cdd961103c7b735d61495
Clianex <clianex.ia@gmail.com>
2025-12-25 18:31:55 +0100

    Add authentication check and redirect in signup page
    Redirect signed-in users to the profile page.

--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,18 @@
 // pages/signup.js
-import { SignUp } from '@clerk/nextjs';
+import { SignUp, useAuth } from '@clerk/nextjs';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const SignUpPage = () => {
+  const { isSignedIn } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push('/profile');  // O la página que desees redirigir al usuario
+    }
+  }, [isSignedIn, router]);
+
   return (
     <div>
       <h1>Crear una cuenta</h1>
