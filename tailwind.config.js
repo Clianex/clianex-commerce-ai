2367481d911e5ec23c84087156429d8770d9820c
Clianex <clianex.ia@gmail.com>
2025-12-25 12:22:59 +0100

    Update Tailwind config for JSX file support
    Configure Tailwind CSS to scan JSX files in the app directory.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx}",  // Asegura que Tailwind se aplique a archivos JSX dentro de "app"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 
