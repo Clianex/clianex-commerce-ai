2f57aa3f50f7e6781fbb34f35212485acb159246
Clianex <clianex.ia@gmail.com>
2025-12-25 19:44:13 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   experimental: {
-    appDir: true, // Activa el App Router si estás usando esta estructura
-  },
-  env: {
-    NEXT_PUBLIC_CLERK_FRONTEND_API: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API,
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY, // Solo se usará en el backend
+    appDir: true, // Muy importante para App Router
   },
 };
 
