584f4791bb709ed4769c00857e05443552505ea5
Clianex <clianex.ia@gmail.com>
2025-12-26 15:27:13 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,13 @@
 import Link from "next/link";
+import Hero from "./components/Hero";
+
+export default function HomePage() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
+}
 
 export default function HomePage() {
   return (
