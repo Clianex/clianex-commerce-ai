f8aa157d63c7f756948b00627ac6f47dbb7c5daf
Clianex <clianex.ia@gmail.com>
2025-12-26 09:14:46 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,jsx}",  // Asegura que Tailwind se aplique a archivos JSX dentro de "app"
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./app/**/*.{js,jsx}"],
+  theme: { extend: {} },
   plugins: [],
-}
-
+};
