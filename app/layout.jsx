da55374adc778196dee0f7c42aa65081f49b1a05
Clianex <clianex.ia@gmail.com>
2025-12-26 16:07:07 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,13 +3,13 @@ import Navbar from "./components/Navbar";
 
 export const metadata = {
   title: "Clianex Commerce AI",
-  description: "Plataforma de comercio impulsada por IA",
+  description: "Plataforma serverless de comercio impulsado por IA",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="bg-black text-white">
+      <body className="bg-black text-white antialiased">
         <Navbar />
         {children}
       </body>
