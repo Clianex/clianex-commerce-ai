26047d90ce22b4c7cb7ead3a51e8052aadcc6cb1
Clianex <clianex.ia@gmail.com>
2025-12-29 14:16:11 +0100

    Update page.jsx

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -1,13 +1,5 @@
-// pages/signin.jsx
-import { SignIn } from '@clerk/nextjs';
+import { SignIn } from "@clerk/nextjs";
 
-const SignInPage = () => {
-  return (
-    <div style={{ maxWidth: '500px', margin: '50px auto', padding: '20px' }}>
-      <h1>Iniciar sesión</h1>
-      <SignIn />
-    </div>
-  );
-};
-
-export default SignInPage;
+export default function Page() {
+  return <SignIn />;
+}
