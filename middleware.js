5c99fcd716e3787f2c62fb37cbef14a989ff6f91
Clianex <clianex.ia@gmail.com>
2025-12-29 14:05:53 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,9 +1,7 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs";
 
 export default clerkMiddleware();
 
 export const config = {
-  matcher: [
-    "/dashboard(.*)",
-  ],
+  matcher: ["/dashboard(.*)"],
 };
