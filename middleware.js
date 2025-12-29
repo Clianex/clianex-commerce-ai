3c5b3adb198f84d161edc4caeb6412df397a5f26
Clianex <clianex.ia@gmail.com>
2025-12-29 22:01:11 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,6 +3,5 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  // Protege dashboard y todas sus subrutas
   matcher: ["/dashboard/:path*"],
 };
