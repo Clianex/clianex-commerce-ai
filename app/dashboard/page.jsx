7b73c85fa01041f7a4657652d03523229e0dc7df
Clianex <clianex.ia@gmail.com>
2025-12-29 18:35:45 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,12 +1,11 @@
-import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
 export default function Dashboard() {
   const { userId } = auth();
-
   return (
     <main style={{ padding: 40 }}>
       <h1>Dashboard</h1>
-      <p>User ID: {userId}</p>
+      <p>User: {userId}</p>
     </main>
   );
 }
