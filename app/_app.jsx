e0a7d1949a0986e6dfbbb3a24119e0e41197f627
Clianex <clianex.ia@gmail.com>
2026-01-17 15:10:03 +0100

    Update _app.jsx

--- app/_app.jsx
+++ app/_app.jsx
@@ -1,7 +1,7 @@
 // pages/_app.js
 import { ClerkProvider } from '@clerk/nextjs';
 import NavBar from '../components/NavBar';
-import '../styles/globals.css';
+import '../app/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
