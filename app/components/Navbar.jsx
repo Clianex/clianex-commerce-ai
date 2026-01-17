a2800a77db5c5b45c93094a0948b2a5c69d48105
Clianex <clianex.ia@gmail.com>
2026-01-17 13:05:32 +0100

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import UserMenu from "./UserMenu";
 
 export default function NavBar() {
   const pathname = usePathname();
