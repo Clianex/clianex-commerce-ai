7301d8e08cc26a089f83098023019b1cd3cdc0e4
Clianex <clianex.ia@gmail.com>
2026-01-17 15:02:29 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,7 @@
 export default function Page() {
   return (
-    <div className="p-10">
-      <h1 className="text-5xl font-bold text-red-500">
-        Tailwind TEST
-      </h1>
-    </div>
+    <h1 className="text-6xl font-bold text-green-500">
+      FUNCIONA
+    </h1>
   );
 }
