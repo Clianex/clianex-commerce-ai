d3033512f3ee805be1fee3b8f63fb86f04ff1bd0
Clianex <clianex.ia@gmail.com>
2026-01-19 16:04:49 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        dark: "#0F172A",
-        card: "#111827",
-        primary: "#38BDF8",
-      },
-      borderRadius: {
-        xl: "1rem",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 };
