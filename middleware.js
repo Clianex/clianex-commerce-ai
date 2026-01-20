6e8d1bed469d3683e4cf49160d8146b4a52e1cc8
Clianex <clianex.ia@gmail.com>
2026-01-20 20:22:41 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -5,7 +5,13 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
+<<<<<<< HEAD
+    "/((?!_next|favicon.ico|public).*)",
+    "/dashboard/:path*",
+    "/marketplace/:path*"
+=======
     "/dashboard/:path*",
     "/((?!_next|static|favicon.ico).*)",
+>>>>>>> fbc312ff808e9d9bcbe1b6bb3c2a294189aa02d7
   ],
 };
