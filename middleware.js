693e336f3e60888763b85960f0fbc82c7e96fb5b
Clianex <clianex.ia@gmail.com>
2026-01-20 17:15:47 +0100

    Add files via upload

--- middleware.js
+++ middleware.js
@@ -1,10 +1,11 @@
+
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  secretKey: process.env.CLERK_SECRET_KEY,
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: [
+    "/dashboard/:path*",
+    "/((?!_next|static|favicon.ico).*)",
+  ],
 };
