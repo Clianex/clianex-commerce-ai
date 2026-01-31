6db83347f2004b617eb9b7fbc28b284dd3b86494
Clianex <clianex.ia@gmail.com>
2026-01-31 15:52:33 +0100

    Delete .next directory

--- .next/dev/static/chunks/app_page_jsx_269a8a3e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_7a283893._.js",
-  "static/chunks/components_layout_Header_jsx_a975e830._.js"
-],
-    source: "dynamic"
-});
--- .next/server/chunks/_next-internal_server_app_api_dashboard_modules_route_actions_787a69f3.js
+++
@@ -1,3 +0,0 @@
-module.exports=[65529,(e,o,d)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_api_dashboard_modules_route_actions_787a69f3.js.map
